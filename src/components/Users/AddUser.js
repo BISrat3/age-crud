5d2be07db658e31deb2f90a4e429a2a6fd1a51a5
bisrat <bisrat.amtataw@gmail.com>
2022-09-30 14:57:23 -0400

    card components

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,25 @@
 import React, {useState} from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css"
+
 
 const AddUser = (props) =>{
+    const [user, setUser] = useState('')
 
     const addUserHandler =(event) =>{
       event.preventDefalut(); 
     }
 
     return(
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username </label>
-            <input type="text" id="username"/> 
-            <label htmlFor="age" >Age (Years)</label>
-            <input id="age" type="number" required/>
-            <button type="submit">Add User</button>
-        </form>
+        <Card className= {classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username </label>
+                <input type="text" id="username"/> 
+                <label htmlFor="age" >Age (Years)</label>
+                <input id="age" type="number" required/>
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
     )
 }
 
