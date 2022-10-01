0b64ce52adf2c9aef9f8d6acc34c62ae7f098730
bisrat <bisrat.amtataw@gmail.com>
2022-10-01 11:53:48 -0400

    Error message

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -3,13 +3,14 @@ import classes from './Button.module.css'
 
 
 const Button = (props) => {
-    return 
+    return (
         <button 
             className={classes.button} 
             type = {props.type || 'button'}
             onCLick= {props.onClick}>
                 {props.children}
          </button>
+    )
 }
 
 export default Button;
