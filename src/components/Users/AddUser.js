79ffbd7040b584ac105e36ace0d029cdd84ee17e
bisrat <bisrat.amtataw@gmail.com>
2022-10-01 11:31:42 -0400

    Error message

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css"
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) =>{
     const [enteredUsername, setEnteredUsername] = useState('')
@@ -31,16 +32,23 @@ const AddUser = (props) =>{
         setEnteredAge(event.target.value);
     }
 
-    return(
+    return (
+        <div>
+        <ErrorModal title="An error occured!" message="Something went Wrong!" />
         <Card className= {classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username </label>
-                <input type="text" id="username" value ={enteredUsername} onChange= {usernameChangeHandler}/> 
+                <input
+                     type="text" id="username" value ={enteredUsername} onChange= {usernameChangeHandler}
+                     /> 
                 <label htmlFor="age" >Age (Years)</label>
-                <input id="age" type="number" value={enteredAge} onChange = {ageChangeHandler} required/>
+                <input 
+                    id="age" type="number" value={enteredAge} onChange = {ageChangeHandler} 
+                    required/>
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
+        </div>
     )
 }
 
